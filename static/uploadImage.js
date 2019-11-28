bf741dd314d2227e6d259d32dd202ce07d2f8f7f
Timmy Silesmo <silesmo@amazon.com>
2019-11-28 19:05:10 +0100

    Added confirmation

--- static/uploadImage.js
+++ static/uploadImage.js
@@ -12,7 +12,15 @@ function uploadImage()
         var data = JSON.stringify({ "name": name, "imageData": binaryString });
         console.log(data);
         $.post("https://rbsxsslare.execute-api.eu-west-1.amazonaws.com/default/uploadImage", data, function(result){
-            $("span").html(result);
+            var div = document.createElement("div");
+            div.style.width = "100px";
+            div.style.height = "100px";
+            div.style.color = "green";
+            div.innerHTML = "Bad guy added!";
+            var element = document.getElementById("buttonInput");
+            element.parentNode.removeChild(element);
+            document.getElementById("main").appendChild(div);
+            
           });
     };
     reader.readAsDataURL(image);
