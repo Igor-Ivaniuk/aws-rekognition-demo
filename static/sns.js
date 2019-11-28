1230bdd458be2928fb2cb5bc730188443deb5098
Igor Ivaniuk <iivaniuk@amazon.com>
2019-11-28 19:32:49 +0100

    All working

--- static/sns.js
+++ static/sns.js
@@ -1,23 +1,30 @@
 // Email ARN
 var topicARN = 'arn:aws:sns:eu-west-1:637530424188:fiveguysemail'
 
-function publishToSNS(message)
-{
-    // Create publish parameters
-    var params = {
-        Message: message,
-        TopicArn: topicARN
-    }
-    // Create promise and SNS service object
-    var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+var sentNotifications = new Set();
 
-    // Handle promise's fulfilled/rejected states
-    publishTextPromise.then(
-        function(data) {
-        console.log(`Message ${params.Message} send sent to the topic ${params.TopicArn}`);
-        console.log("MessageID is " + data.MessageId);
-        }).catch(
-        function(err) {
-        console.error(err, err.stack);
-        });
+function publishToSNS(faceId, message) {
+    if (sentNotifications.has(faceId)) {
+        // Do nothign to not spam emails
+    } else {
+        // Create publish parameters
+        var params = {
+            Message: message,
+            TopicArn: topicARN
+        }
+        // Create promise and SNS service object
+        var publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
+
+        // Handle promise's fulfilled/rejected states
+        publishTextPromise.then(
+            function (data) {
+                console.log(`Message ${params.Message} send sent to the topic ${params.TopicArn}`);
+                console.log("MessageID is " + data.MessageId);
+            }).catch(
+                function (err) {
+                    console.error(err, err.stack);
+                });
+        sentNotifications.add(faceId);
+        console.log("Email sent");
+    }
 }
